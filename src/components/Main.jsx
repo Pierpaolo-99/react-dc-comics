439653fd15b6207e2a4a1fe453857779d0176401
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-03-20 16:07:14 +0100

    add Main component and integrate into App; update styles for layout

---
+++ src/components/Main.jsx
@@ -0,0 +1,49 @@
+export default function Main() {
+
+    return (
+        <main>
+
+            <div className="container_contents">
+                <div className="content">
+                    <h2>Content goes here</h2>
+                </div>
+            </div>
+
+            <div className="container_shop">
+                <ul className="shop">
+                    <li>
+                        <a href="">
+                            <img src="/img/buy-comics-digital-comics.png" alt="" />
+                            <span>DIGITAL COMICS</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="/img/buy-comics-merchandise.png" alt="" />
+                            <span>DC MERCHANDISE</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="/img/buy-comics-subscriptions.png" alt="" />
+                            <span>SUBSCRIPTION</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="/img/buy-comics-shop-locator.png" alt="" />
+                            <span>COMIC SHOP LOCATOR</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="/img/buy-comics-power-visa.svg" alt="" />
+                            <span>DC POWER VISA</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </main>
+    )
+}
