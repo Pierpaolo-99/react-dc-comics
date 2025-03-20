79ab1424d695df36029be9b337bd0ce7f56c905c
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-03-20 15:14:46 +0100

    initialize project with React, Vite, and essential assets

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+export default function Header() {
+
+    return (
+        <header>
+
+            <div className="flex-container">
+
+                <div className="logo">
+                    <img src="/img/dc-logo.png" alt="" />
+                </div>
+
+                <div>
+                    <ul className="menu">
+                        <li><a href="">CHARACTERS</a></li>
+                        <li><a className="active" href="">COMICS</a></li>
+                        <li><a href="">MOVIES</a></li>
+                        <li><a href="">TV</a></li>
+                        <li><a href="">GAMES</a></li>
+                        <li><a href="">COLLECTIBLES</a></li>
+                        <li><a href="">VIDEOS</a></li>
+                        <li><a href="">FANS</a></li>
+                        <li><a href="">NEW</a></li>
+                        <li><a href="">SHOP</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </header>
+    )
+}
