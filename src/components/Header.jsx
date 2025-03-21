7d533a96a6741b0da127d477122d3d027fe39a14
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-03-21 16:21:30 +0100

    refactor Header component; simplify menu structure by removing unnecessary div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,20 +9,19 @@ export default function Header() {
                     <img src="/img/dc-logo.png" alt="" />
                 </div>
 
-                <div>
-                    <ul className="menu">
-                        <li><a href="">CHARACTERS</a></li>
-                        <li><a className="active" href="">COMICS</a></li>
-                        <li><a href="">MOVIES</a></li>
-                        <li><a href="">TV</a></li>
-                        <li><a href="">GAMES</a></li>
-                        <li><a href="">COLLECTIBLES</a></li>
-                        <li><a href="">VIDEOS</a></li>
-                        <li><a href="">FANS</a></li>
-                        <li><a href="">NEW</a></li>
-                        <li><a href="">SHOP</a></li>
-                    </ul>
-                </div>
+                <ul className="menu">
+                    <li><a href="">CHARACTERS</a></li>
+                    <li><a className="active" href="">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">VIDEOS</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEW</a></li>
+                    <li><a href="">SHOP</a></li>
+                </ul>
+
             </div>
 
         </header>
